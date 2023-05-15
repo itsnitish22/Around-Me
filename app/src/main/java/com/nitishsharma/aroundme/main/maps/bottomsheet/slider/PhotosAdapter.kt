edae5573ea99aed335e3b320cc0ac47c1a6f31d5
Nitish Sharma <nitish.sharma1186@gmail.com>
2023-05-15 23:49:19 +0530

    [tweaks] viewbinding in adapter

--- app/src/main/java/com/nitishsharma/aroundme/main/maps/bottomsheet/slider/PhotosAdapter.kt
+++ app/src/main/java/com/nitishsharma/aroundme/main/maps/bottomsheet/slider/PhotosAdapter.kt
@@ -1,36 +1,30 @@
 package com.nitishsharma.aroundme.main.maps.bottomsheet.slider
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.nitishsharma.aroundme.R
+import com.nitishsharma.aroundme.databinding.ItemSliderBinding
 
 class PhotosAdapter(
-    slideList: ArrayList<String>
+    private val slideList: ArrayList<String>
 ) : RecyclerView.Adapter<PhotosAdapter.PhotosViewHolder>() {
 
-    var sliderList: ArrayList<String> = slideList
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotosViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_slider, parent, false)
-        return PhotosViewHolder(view)
+        val binding = ItemSliderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PhotosViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: PhotosViewHolder, position: Int) {
-        val placeImage = sliderList[position] //get the url from item position
-        Glide.with(holder.imageView.context).load(placeImage)
-            .into(holder.imageView) //load it using glide in the view
+        val placeImage = slideList[position]
+        Glide.with(holder.binding.root.context).load(placeImage)
+            .into(holder.binding.placeIv)
     }
 
     override fun getItemCount(): Int {
-        return if (sliderList.size >= 6) 6 else sliderList.size //if the size of array is more than or equal to 6, show only 6 images, otherwise show whatever size is available
+        return if (slideList.size >= 6) 6 else slideList.size
     }
 
-    class PhotosViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var imageView: ImageView = itemView.findViewById(R.id.placeIv)
-    }
+    inner class PhotosViewHolder(val binding: ItemSliderBinding) :
+        RecyclerView.ViewHolder(binding.root)
 }
